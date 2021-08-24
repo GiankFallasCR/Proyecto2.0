971b5854e8643bb40337634b5fb8aaa264f9464c
fbogantes <78193073+fbogantes@users.noreply.github.com>
2021-08-24 02:30:54 -0600

    Cambio hecho por Frey

--- search.php
+++ search.php
@@ -14,7 +14,7 @@ include './library/consulSQL.php';
        <br>
         <div class="container">
             <div class="page-header">
-              <h1>BÚSQUEDA DE destinoS <small class="tittles-pages-logo">STORE</small></h1>
+              <h1>Busqueda de destinos <small class="tittles-pages-logo">ViajiTico</small></h1>
             </div>
             <div class="container-fluid">
               <div class="row">
