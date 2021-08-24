8829e87beecabda4fa3af5d192299d1b3228b477
fbogantes <78193073+fbogantes@users.noreply.github.com>
2021-08-24 01:55:09 -0600

    Cambio hecho por Frey

--- destino.php
+++ destino.php
@@ -10,75 +10,77 @@ include './library/consulSQL.php';
 </head>
 <body id="container-page-destino">
     <?php include './inc/navbar.php'; ?>
-    <section id="store">
-       <br>
-        <div class="container">
-            <div class="page-header">
-              <h1>Destinos <small class="tittles-pages-logo">Viajitico</small></h1>
-            </div>
-              <div class="container-fluid">
-                <div class="row">
-                  
-                  <div class="col-xs-12 col-md-4 col-md-offset-4">
-                    <form action="./search.php" method="GET">
-                      <div class="form-group">
-                        <div class="input-group">
-                          <span class="input-group-addon"><i class="fa fa-search" aria-hidden="true"></i></span>
-                          <input type="text" id="addon1" class="form-control" name="term" required="" title="Escriba nombre o Provincia del destino">
-                          <span class="input-group-btn">
-                              <button class="btn btn-info btn-raised" type="submit">Buscar</button>
-                          </span>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+    <div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+            <br><br>
+            <div class="panel panel-info">
+              <div class="panel-heading text-center"><h4>destinos en tienda</h4></div>
+                <div class="table-responsive">
+                  <table class="table table-striped table-hover">
+                      <thead class="">
+                          <tr>
+                          	  <th class="text-center">#</th>
+                              <th class="text-center">Código</th>
+                              <th class="text-center">Nombre</th>
+                              <th class="text-center">Precio</th>
+                              <th class="text-center">Canton</th>
+                              <th class="text-center">Provincia</th>
+                              <th class="text-center">cantidad</th>
+                              <th class="text-center">Estado</th>
+                              <th class="text-center">Actualizar</th>
+                              <th class="text-center">Eliminar</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                        	$mysqli = mysqli_connect(SERVER, USER, PASS, BD);
+							mysqli_set_charset($mysqli, "utf8");
+
+							$pagina = isset($_GET['pag']) ? (int)$_GET['pag'] : 1;
+							$regpagina = 30;
+							$inicio = ($pagina > 1) ? (($pagina * $regpagina) - $regpagina) : 0;
+
+							$destinos=mysqli_query($mysqli,"SELECT SQL_CALC_FOUND_ROWS * FROM destino LIMIT $inicio, $regpagina");
+
+							$totalregistros = mysqli_query($mysqli,"SELECT FOUND_ROWS()");
+							$totalregistros = mysqli_fetch_array($totalregistros, MYSQLI_ASSOC);
+
+							$numeropaginas = ceil($totalregistros["FOUND_ROWS()"]/$regpagina);
+
+							$cr=$inicio+1;
+                            while($prod=mysqli_fetch_array($destinos, MYSQLI_ASSOC)){
+                        ?>
+                        <tr>
+                        	<td class="text-center"><?php echo $cr; ?></td>
+                        	<td class="text-center"><?php echo $prod['CodigoDestino']; ?></td>
+                        	<td class="text-center"><?php echo $prod['NombreDestino']; ?></td>
+                        	<td class="text-center"><?php echo $prod['Precio']; ?></td>
+                        	<td class="text-center"><?php echo $prod['Canton']; ?></td>
+                        	<td class="text-center"><?php echo $prod['Provincia']; ?></td>
+                        	<td class="text-center"><?php echo $prod['cantidad']; ?></td>
+                        	<td class="text-center">
+                        		<?php echo $prod['Estado']; ?>
+                        	</td>
+                        	<td class="text-center">
+                        		<a href="configAdmin.php?view=destinoinfo&code=<?php echo $prod['CodigoDestino']; ?>" class="btn btn-raised btn-xs btn-success">Actualizar</a>
+                        	</td>
+                        	<td class="text-center">
+                        		<form action="process/delprod.php" method="POST" class="FormCatElec" data-form="delete">
+                        			<input type="hidden" name="prod-code" value="<?php echo $prod['CodigoDestino']; ?>">
+                        			<button type="submit" class="btn btn-raised btn-xs btn-danger">Eliminar</button>	
+                        		</form>
+                        	</td>
+                        </tr>
+                        <?php 
+                        	$cr++;
+                        	}
+                        ?>
+                      </tbody>
+                  </table>
                 </div>
-              </div>
-
-              <div class="row">
-                <?php
-                  $mysqli = mysqli_connect(SERVER, USER, PASS, BD);
-                  mysqli_set_charset($mysqli, "utf8");
-
-                  $pagina = isset($_GET['pag']) ? (int)$_GET['pag'] : 1;
-                  $regpagina = 20;
-                  $inicio = ($pagina > 1) ? (($pagina * $regpagina) - $regpagina) : 0;
-
-                  $consultar_destinos=mysqli_query($mysqli,"SELECT SQL_CALC_FOUND_ROWS * FROM destino WHERE cantidad > 0 AND Estado='Activo' LIMIT $inicio, $regpagina");
-
-                  $totalregistros = mysqli_query($mysqli,"SELECT FOUND_ROWS()");
-                  $totalregistros = mysqli_fetch_array($totalregistros, MYSQLI_ASSOC);
-        
-                  $numeropaginas = ceil($totalregistros["FOUND_ROWS()"]/$regpagina);
-
-                ?>
-                    <div class="col-xs-12 col-sm-6 col-md-4">
-                         <div class="thumbnail">
-                           <img class="img-destino" src="./assets/img-destinos/<?php if($destino['Imagen']!="" && is_file("./assets/img-destinos/".$destino['Imagen'])){ echo $destino['Imagen']; }else{ echo "default.png"; } ?>
-                           ">
-                           <div class="caption">
-                             <h3><?php echo $destino['Provincia']; ?></h3>
-                             <p><?php echo $destino['NombreDestino']; ?></p>
-                             <?php if($destino['Descuento']>0): ?>
-                             <p>
-                             <?php
-                             $pref=number_format($destino['Precio']-($destino['Precio']*($destino['Descuento']/100)), 2, '.', '');
-                             echo $destino['Descuento']."% descuento: $".$pref; 
-                             ?>
-                             </p>
-                             <?php else: ?>
-                              <p>$<?php echo $destino['Precio']; ?></p>
-                             <?php endif; ?>
-                             <p class="text-center">
-                                 <a href="infoProd.php?CodigoDestino=<?php echo $destino['CodigoDestino']; ?>" class="btn btn-primary btn-raised btn-sm btn-block"><i class="fa fa-plus"></i>&nbsp; Detalles</a>
-                             </p>
-
-                           </div>
-                         </div>
-                     </div>     
-\
-                <div class="clearfix"></div>
-                <div class="text-center">
+                <?php if($numeropaginas>=1): ?>
+              	<div class="text-center">
                   <ul class="pagination">
                     <?php if($pagina == 1): ?>
                         <li class="disabled">
@@ -86,7 +88,24 @@ include './library/consulSQL.php';
                                 <span aria-hidden="true">&laquo;</span>
                             </a>
                         </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="configAdmin.php?view=destinolist&pag=<?php echo $pagina-1; ?>">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
                     <?php endif; ?>
+
+
+                    <?php
+                        for($i=1; $i <= $numeropaginas; $i++ ){
+                            if($pagina == $i){
+                                echo '<li class="active"><a href="configAdmin.php?view=destinolist&pag='.$i.'">'.$i.'</a></li>';
+                            }else{
+                                echo '<li><a href="configAdmin.php?view=destinolist&pag='.$i.'">'.$i.'</a></li>';
+                            }
+                        }
+                    ?>
                     
 
                     <?php if($pagina == $numeropaginas): ?>
@@ -95,12 +114,19 @@ include './library/consulSQL.php';
                                 <span aria-hidden="true">&raquo;</span>
                             </a>
                         </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="configAdmin.php?view=destinolist&pag=<?php echo $pagina+1; ?>">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
                     <?php endif; ?>
                   </ul>
                 </div>
-
+                <?php endif; ?>
+            </div>
         </div>
-    </section>
-    <?php include './inc/footer.php'; ?>
+	</div>
+</div>
 </body>
 </html>
